1c80cea10626328c3b7625322ec510149247615b
Matt Chirco <matteochirco@gmail.com>
2026-01-17 14:09:07 -0700

    nobody user

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,7 @@
 #In a terminal run
 #curl -fsSL https://raw.githubusercontent.com/mat926/archinstall-scripts/refs/heads/main/postinstall.sh | bash
 USERNAME=$(getent passwd 1000 | cut -d: -f1)
+NOBODY="nobody"
 
 # Remove some of the bloat installed with KDE
 pacman -Rns --noconfirm ark kate #konsole
@@ -13,9 +14,9 @@ pacman -Rns --noconfirm ark kate #konsole
 pacman -S --noconfirm --needed base-devel
 cd /tmp
 git clone https://aur.archlinux.org/paru.git paru-tmp
-chown -R $USERNAME:$USERNAME paru-tmp
+chown -R $NOBODY:$NOBODY paru-tmp
 cd paru-tmp
-#runuser -u $USERNAME -- makepkg -si --noconfirm
+runuser -u $NOBODY -- makepkg -si --noconfirm
 
 # Clean up after
 cd /tmp
