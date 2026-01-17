41eec5d3bfc4ce06d755517ddd18de508dae3ec2
Matt Chirco <matteochirco@gmail.com>
2026-01-17 13:00:42 -0700

    installl paru

--- postinstall.sh
+++ postinstall.sh
@@ -3,5 +3,19 @@
 #curl -fsSL https://raw.githubusercontent.com/mat926/archinstall-scripts/refs/heads/main/postinstall.sh | bash
 
 # Remove some of the bloat installed with KDE
-pacman -Rns --noconfirm ark kate konsole
-echo "System setup finished successfully" >> ~/Desktop/postinstall_log.txt
+pacman -Rns --noconfirm ark kate #konsole
+
+
+#######################################################
+## Install AUR helpers
+#######################################################
+cd /tmp
+pacman -S --noconfirm--needed base-devel
+git clone https://aur.archlinux.org/paru.git paru-tmp
+chown -R testuser:testuser paru-tmp
+cd paru-tmp
+runuser -u testuser -- makepkg -si --noconfirm
+
+# Clean up after
+cd /tmp
+rm -rf paru-tmp
