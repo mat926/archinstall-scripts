3915d160f9f6ade1331346d606dd7b2a272bcc14
Matt Chirco <matteochirco@gmail.com>
2026-01-17 13:31:21 -0700

    get username

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,7 @@
 #USAGE
 #In a terminal run
 #curl -fsSL https://raw.githubusercontent.com/mat926/archinstall-scripts/refs/heads/main/postinstall.sh | bash
+USERNAME=$(getent passwd 1000 | cut -d: -f1)
 
 # Remove some of the bloat installed with KDE
 pacman -Rns --noconfirm ark kate #konsole
@@ -9,12 +10,12 @@ pacman -Rns --noconfirm ark kate #konsole
 #######################################################
 ## Install AUR helpers
 #######################################################
-cd /tmp
 pacman -S --noconfirm--needed base-devel
+cd /tmp
 git clone https://aur.archlinux.org/paru.git paru-tmp
-chown -R testuser:testuser paru-tmp
+chown -R $USERNAME:$USERNAME paru-tmp
 cd paru-tmp
-runuser -u testuser -- makepkg -si --noconfirm
+runuser -u $USERNAME -- makepkg -si --noconfirm
 
 # Clean up after
 cd /tmp
