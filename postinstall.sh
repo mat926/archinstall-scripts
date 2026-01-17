86482423eeffcc712cbac9789857cca03ab501a8
Matt Chirco <matteochirco@gmail.com>
2026-01-17 16:19:02 -0700

    remove custom command

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,9 @@ USERNAME=$(getent passwd 1000 | cut -d: -f1)
 NOBODY="nobody"
 
 # Remove some of the bloat installed with KDE
-pacman -Rns --noconfirm ark kate #konsole
+pacman -Rns --noconfirm ark kate discover #konsole
+
+
 
 
 #######################################################
@@ -14,9 +16,13 @@ pacman -Rns --noconfirm ark kate #konsole
 pacman -S --noconfirm --needed base-devel
 cd /tmp
 git clone https://aur.archlinux.org/paru.git paru-tmp
-chown -R $NOBODY:$NOBODY paru-tmp
+chown -R $USERNAME:$USERNAME paru-tmp
 cd paru-tmp
-runuser -u $NOBODY -- makepkg -si --noconfirm
+
+# As root, add a temporary sudoers drop-in file to allow passwordless sudo for the user
+echo "$USERNAME ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers.d/temp-sudoers
+
+runuser -u $USERNAME -- makepkg -si --noconfirm
 
 # Clean up after
 cd /tmp
